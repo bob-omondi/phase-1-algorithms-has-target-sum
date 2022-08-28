5e8505614325fcd2e6dcc8a16b6f500faf63a148
+ <+>
2022-08-28 15:46:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function hasTargetSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
   // Write your algorithm here
 }
 
